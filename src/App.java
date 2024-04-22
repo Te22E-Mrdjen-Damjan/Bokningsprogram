68714d6aa02f58c8de891327d722768de042cabe
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2024-04-22 09:31:11 +0200

    Gjort meny

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
+
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tb = new Scanner(System.in);
+        int[]array = new int[20];
+        
+        System.out.println("1.Boka Plats");
+        System.out.println("2.Visa lediga platser");
+        System.out.println("3.Visa Vinst");
+        System.out.println("4.avsluta");
+
+        int Val = tb.nextInt();
+
+        if (Val==1){
+        
+
+        }
+        else if (Val==2) {
+
+
+        }
+        else if (Val==3){
+
+
+        }
+
+        else if (Val==4){
+
+
+        }
+
+
     }
 }
